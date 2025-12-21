1262077be5e130bec9bed2e11387233df1a0565e
Rifal21 <giantjou77@gmail.com>
2025-12-21 16:21:10 +0700

    fix: https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
